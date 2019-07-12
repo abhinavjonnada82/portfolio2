57018606cb37081f83b5db62d7c8ee963ae2eee8
ABHINAV JONNADA <30497847+abhinavjonnada82@users.noreply.github.com>
2019-07-12 02:37:42 -0400

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -6,8 +6,8 @@
         <br />
         <h2 class="subtitle">
           👋 I'm Abhinav, I received my Bachelor's degree (CS/CE) from Wichita State 🏫.
-          I have more than a year of experience in building custom software solutions
-          for a leading startup, regional company & giant corporation 🏢.
+          I have around 2 years of experience in building custom software solutions
+          for a leading startup, regional company & giant corporation 🏢. Demonstrated experience in various software methodologies and their implementation.
           My work experience 👔 ranges from building enterprise-level applications
           using Python
           <i class="fab fa-python"></i> to web applications using React JS
