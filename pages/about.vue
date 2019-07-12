793a2cdfaba783061ac4fc20def546c70f325107
ABHINAV JONNADA <30497847+abhinavjonnada82@users.noreply.github.com>
2019-07-12 03:41:55 -0400

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -14,7 +14,7 @@
           <i class="fab fa-react"></i>, Vue JS
           <i class="fab fa-vuejs"></i> & Node JS
           <i class="fab fa-js"></i>.
-          In my spare time, I like staying active ⚽ & I'm a movie buff 🎦.
+          Also, I like staying active ⚽ & I'm a movie buff 🎦.
         </h2>
         <img
           src="https://render.bitstrips.com/v2/cpanel/f743b69a-9fa6-40f1-bdcd-d52bcf11ddee-a66e106b-fb23-4725-a748-abdf5fde23f2-v1.png?transparent=1&palette=1"
